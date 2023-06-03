f2cb7e173c32c1844c8658f83e0eb5c632bbdc8f
Theam-Dev <phearakroun@gmail.com>
2023-06-03 12:43:43 +0700

    dd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {SlideshowLightbox} from 'lightbox.js-react'
+import 'lightbox.js-react/dist/index.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>React Lightbox</h2>
+      <SlideshowLightbox theme="lightbox" className="lightbox">
+          <img className='w-full rounded' src='img/1.jpg' width={200}/>
+          <img className='w-full rounded' src='img/2.jpg' width={200}/>  
+          <img className='w-full rounded' src='img/3.jpg' width={200}/>
+          <img className='w-full rounded' src='img/4.jpg' width={200}/>
+   
+        </SlideshowLightbox> 
       </header>
     </div>
   );
